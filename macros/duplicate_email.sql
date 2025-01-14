6dd614c75d099a751c95d5e6d7c0ce8ec39675b0
Alejandra Oliver <alejandra.oliver@civica-soft.com>
2025-01-14 13:16:03 +0000

    Silver y bronce

--- macros/duplicate_email.sql
+++ macros/duplicate_email.sql
@@ -1,21 +1,19 @@
-{% test duplicate_email(model,column_name) %}
+{% test duplicate_email(model, email_column, first_name_column, last_name_column) %}
+    
+    WITH combined_data AS (
+        SELECT CONCAT({{ first_name_column }}, ' ', {{ last_name_column }}) AS contact_name, {{ email_column }} AS email
+        FROM {{ model }}
+    ),
+    duplicate_emails AS (
+        SELECT email
+        FROM combined_data
+        GROUP BY email
+        HAVING COUNT(DISTINCT contact_name) > 1  -- Verifica si más de un contacto tiene el mismo email
+    )
 
-WITH combined_data AS (
-    SELECT contact_name, email
-    FROM {{ ref('stg_catalog_suppliers') }}
-    UNION ALL
-    SELECT CONCAT(first_name, ' ', last_name) AS contact_name, email
-    FROM {{ ref('stg_catalog_customers') }}
-),
-duplicate_emails AS (
-    SELECT email
-    FROM combined_data
-    GROUP BY email
-    HAVING COUNT(DISTINCT contact_name) > 1  -- Verifica si más de un contacto tiene el mismo email
-)
-
-SELECT *
-FROM duplicate_emails
+    SELECT *
+    FROM duplicate_emails
 
 {% endtest %}
+
 
